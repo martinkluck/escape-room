08ab733437142e0df48119156eaaf9d861313ae5
Martin Gustavo Kluck <martin.kluck@sesocio.com>
2020-10-24 12:55:52 -0300

    feat: configure tailwind with storybook

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,8 @@
 
 <style>
 html {
-  /* font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif; */
-  font-family: 'Indie Flower', 'Source Sans Pro', -apple-system,
-    BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -17,8 +15,6 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-  background-color: black;
-  color: white;
 }
 
 *,
